2240a88fcbca8c60de5ae2018571c22a550e7e43
bachmann <bachmann.matt@gmail.com>
2015-09-14 00:18:59 -0400

    initial working version

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # java9_kernel
 An ipython kernel for java 9
+
+## Expectation Setting
+This is an experimental ipython kernel written with an experimental repl written for
+a unreleased java version. Don't expect this to be production ready.
+
+That being said. File issues and ill do my best.
+
+##Requirements
+
+Install [java9](http://www.oracle.com/technetwork/articles/java/ea-jsp-142245.html)
+
+Find or build yourself a kulla.jar 
+
+[Out of date build guide](http://www.jclarity.com/2015/04/15/java-9-repl-getting-started-guide/)
+
+[This link has a link to the jar](http://mythoughtsjdk.blogspot.com/2015/04/playing-with-java-9-and-repl-ljc-oracle.html)
+
+This kernel expects two environment variables defined
+
+```
+KULLA_HOME - The locaiton of kulla.jar
+
+JAVA_9_HOME - like JAVA_HOME but pointing to a java 9 environment
+```
+
+##Installing the kernel
+
+Assuming you have cloned the repo and got all the requirements above setup
+
+edit kernel.json replacing PATH_TO_javakernel to the location of the javakernel directory
+ 
+```
+mkdir ~/.ipython/kernels/java/
+
+cp <location of your edited kernel.json> ~/.ipython/kernels/java/
+```
+
+For example my kernel.json looks like this
+
+```
+{
+ "argv": ["python3", "/Users/bachmann/Code/java9_kernel/javakernel",
+          "-f", "{connection_file}"],
+ "display_name": "Java 9",
+ "language": "java"
+}
+```
+
+If all worked you should be able to run the kernel
+
+```
+ ipython console --kernel java
+Jupyter Console 4.0.2
+
+[ZMQTerminalIPythonApp] Loading IPython extension: storemagic
+java version "1.9.0-ea"
+Java(TM) SE Runtime Environment (build 1.9.0-ea-b71)
+Java HotSpot(TM) 64-Bit Server VM (build 1.9.0-ea-b71, mixed mode)
+
+In [1]: System.out.println("OMG")
+ System.out.println("OMG")
+OMG
+```
+
+It should work in a notebook as well
+
+![Notebook Screenshot](notebook.png?raw=true)
---
+++ javakernel/__main__.py
@@ -0,0 +1,96 @@
+from subprocess import check_output
+import signal
+from ipykernel.ipkernel import KernelBase
+import os
+from pexpect import replwrap, EOF
+
+
+class JavaKernel(KernelBase):
+    implementation = 'java_kernel'
+    implementation_version = 0.1
+    langauge = "java"
+    language_version = "1.9.0-ea"
+    language_info = {'name': 'java',
+                     'mimetype': 'application/java-vm',
+                     'file_extension': '.class'}
+
+    _JAVA_COMMAND = '{}/bin/java'.format(os.environ['JAVA_9_HOME'])
+    _KULLA_LOCATION = os.environ['KULLA_HOME']
+
+    def __init__(self, **kwargs):
+        super(JavaKernel, self).__init__(**kwargs)
+        self._banner = None
+        self._start_java_repl()
+
+    @property
+    def banner(self):
+        if self._banner is None:
+            self._banner = check_output([self._JAVA_COMMAND, '-version']).decode('utf-8')
+        return self._banner
+
+    def _start_java_repl(self):
+        sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
+        try:
+            self.javawrapper = replwrap.REPLWrapper(
+                "{} -jar {}".format(
+                    self._JAVA_COMMAND,
+                    self._KULLA_LOCATION
+                ),
+                u'->',
+                None
+            )
+        finally:
+            signal.signal(signal.SIGINT, sig)
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+        """
+        :param code:
+            The code to be executed.
+        :param silent:
+            Whether to display output.
+        :param store_history:
+            Whether to record this code in history and increase the execution count.
+            If silent is True, this is implicitly False.
+        :param user_expressions:
+            Mapping of names to expressions to evaluate after the code has run. You can ignore this if you need to.
+        :param allow_stdin:
+            Whether the frontend can provide input on request
+        :return:
+            dict https://ipython.org/ipython-doc/dev/development/messaging.html#execution-results
+        """
+        if not code.strip():
+            return {'status': 'ok', 'execution_count': self.execution_count,
+                    'payload': [], 'user_expressions': {}}
+
+        interrupted = False
+        try:
+            output = self.javawrapper.run_command(code.rstrip(), timeout=None)
+        except KeyboardInterrupt:
+            self.javawrapper.child.sendintr()
+            interrupted = True
+            self.javawrapper._expect_prompt()
+            output = self.javawrapper.child.before
+        except EOF:
+            output = self.javawrapper.child.before + 'Restarting java'
+            self._start_java_repl()
+
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': output}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        if interrupted:
+            return {'status': 'abort', 'execution_count': self.execution_count}
+
+        exitcode = "|  Error: " in output
+
+        if exitcode:
+            return {'status': 'error', 'execution_count': self.execution_count,
+                    'ename': '', 'evalue': output, 'traceback': []}
+        else:
+            return {'status': 'ok', 'execution_count': self.execution_count,
+                    'payload': [], 'user_expressions': {}}
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=JavaKernel)
